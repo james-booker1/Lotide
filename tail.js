f8e3583c008499076282a5a94cd636e48f5073d3
James Booker <james.e.booker@gmail.com>
2022-05-02 21:16:12 -0700

    Head push

---
+++ tail.js
@@ -0,0 +1,18 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed:   ${actual}   !==   ${expected}`);
+  }
+};
+
+const tail = function(){
+let newArray = words.slice(1, 3) 
+console.log(newArray)
+}
+
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); 
+assertEqual(words.length, 3);
+
+
