b38e946434d209b3e05ba1a0255cdc7f6c884268
James Booker <james.e.booker@gmail.com>
2022-05-03 21:29:09 -0700

    eqArrays finished

---
+++ eqArrays.js
@@ -0,0 +1,27 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed:   ${actual}   !==   ${expected}`);
+  }
+};
+
+const eqArrays = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
