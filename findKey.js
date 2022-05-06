3696a035764381cc62201ae581f64f3e095cd05f
James Booker <james.e.booker@gmail.com>
2022-05-05 18:12:09 -0700

    upload

---
+++ findKey.js
@@ -0,0 +1,43 @@
+const assertEqual = function (actual, expected) {
+  if (actual === expected) {
+    console.log(`🟢Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed:   ${actual}   !==   ${expected}`);
+  }
+};
+
+const findKey = function (object, callback) {
+  for (let key in object) {
+    if (callback(object[key])) {
+      return key;
+    }
+  }
+  return undefined;
+};
+
+findKey(
+  {
+    "Blue Hill": { stars: 1 },
+    Akaleri: { stars: 3 },
+    noma: { stars: 2 },
+    elBulli: { stars: 3 },
+    Ora: { stars: 2 },
+    Akelarre: { stars: 3 },
+  },
+  (x) => x.stars === 2
+);
+
+assertEqual(
+  findKey(
+    {
+      James: { noOfFeet: 10 },
+      Bobn: { noOfFeet: 9 },
+      Peter: { noOfFeet: 8 },
+      Kat: { noOfFeet: 9 },
+      Mike: { noOfFeet: 7 },
+      Lisa: { noOfFeet: 7 },
+    },
+    (x) => x.noOfFeet === 7
+  ),
+  "Mike"
+);
---
+++ takeUntil.js
@@ -0,0 +1,50 @@
+const assertArraysEqual = function (actual, expected) {
+  if (eqArrays(actual, expected)) {
+    console.log(`🟢Assertion Passed:  ${actual}  ===   ${expected}`);
+  } else {
+    console.log(`🔴Assertion Failed:   ${actual}   !==   ${expected}`);
+  }
+};
+
+const eqArrays = function (actual, expected) {
+  if (actual.length !== expected.length) {
+    return false;
+  }
+  for (i = 0; i < actual.length; i++) {
+    if (actual[i] !== expected[i]) {
+      return false;
+    }
+  }
+  return true;
+};
+
+const takeUntil = function (array, callback) {
+  const empty = [];
+  for (let item of array) {
+    if (!callback(item)) {
+      empty.push(item);
+    } else return empty;
+  }
+};
+
+const data1 = [1, 2, 5, 7, 2, -1, 2, 4, 5];
+const results1 = takeUntil(data1, (x) => x < 0);
+console.log(results1);
+
+console.log("---");
+
+const data2 = [
+  "I've",
+  "been",
+  "to",
+  "Hollywood",
+  ",",
+  "I've",
+  "been",
+  "to",
+  "Redwood",
+];
+const results2 = takeUntil(data2, (x) => x === ",");
+console.log(results2);
+
+const test = [6, 8, 10, 11, 6, 12];
