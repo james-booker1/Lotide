70ebc283ceeb51cea4fabc79f840afd11d4742db
James Booker <james.e.booker@gmail.com>
2022-05-09 13:49:34 -0700

    tail test refactoring including test

--- tail.js
+++ tail.js
@@ -1,16 +1,9 @@
-const assertEqual = function(actual, expected) {
-  if (actual === expected) {
-    console.log(`🟢Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else {
-    console.log(`🔴Assertion Failed:   ${actual}   !==   ${expected}`);
-  }
-};
+const assertEqual = require('./assertEqual')
 
-const tail = function() {
+
+const tail = function(words) {
   let newArray = words.slice(1);
   console.log(newArray);
 };
 
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words);
-assertEqual(words.length, 3);
+module.exports = tail;
