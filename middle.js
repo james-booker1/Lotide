a278df9b1c63428b0e0e799696ee02dde4d3341d
James Booker <james.e.booker@gmail.com>
2022-05-09 14:04:03 -0700

    middle refactoring with testing

--- middle.js
+++ middle.js
@@ -1,22 +1,7 @@
-const assertArraysEqual = function (actual, expected) {
-  if (eqArrays(actual, expected)) {
-    console.log(`🟢Assertion Passed:  ${actual}  ===   ${expected}`);
-  } else {
-    console.log(`🔴Assertion Failed:   ${actual}   !==   ${expected}`);
-  }
-};
+const assertArraysEqual = require('./assertArraysEqual')
+const eqArrays = require('./eqArrays')
 
-const eqArrays = function (actual, expected) {
-  if (actual.length !== expected.length) {
-    return false;
-  }
-  for (i = 0; i < actual.length; i++) {
-    if (actual[i] !== expected[i]) {
-      return false;
-    }
-  }
-  return true;
-};
+
 
 const middle = function (array) {
   //console.log(Math.floor(arr.length / 2));
@@ -37,5 +22,7 @@ const middle = function (array) {
     return empty;
   }
 };
-console.log(middle([10, 20, 30, 40, 50]));
-console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
+
+
+
+module.exports = middle
---
+++ test/middleTest.js
@@ -0,0 +1,4 @@
+const middle = require('../middle')
+
+console.log(middle([10, 20, 30, 40, 50]));
+console.log(middle([1, 2, 3, 4, 5, 6, 7, 8]));
